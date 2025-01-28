d248d8601c83b0b937f6fa709e3054a8c8aaa2c4
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2025-01-28 12:52:54 +0300

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-center text-green-600">React & Tailwind & React Router Dom with Vite Template</h1>
+    </div>
+  );
+}
+
+export default App;
